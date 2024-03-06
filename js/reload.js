72e39d6137f38d32793fab24113ca6fcdc746d31
manellasledj <162429121+manellasledj@users.noreply.github.com>
2024-03-06 14:33:53 +0100

    Add files via upload

---
+++ js/reload.js
@@ -0,0 +1,5 @@
+setTimeout(function() {
+    document.getElementById('socket').style.display = 'none'; // يختفي العنصر reload
+    document.getElementById('container').style.display = 'block'; // يظهر العنصر home
+  }, 1500); // مدة التأخير بالميلي ثانية (ثلاث ثواني = 3000 ميلي ثانية)
+  
